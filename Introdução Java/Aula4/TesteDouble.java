666b41da989500ca643e8912286dd0d560ddd976
brunakobayachi <brunakobayachi@gmail.com>
2022-10-10 10:24:58 -0300

    feat aula4

---
+++ Introdução Java/Aula4/TesteDouble.java
@@ -0,0 +1,8 @@
+public class TesteDouble {
+
+    public static void main(String[] args) {
+        double salario;
+        salario = 1230.22;
+        System.out.println("meu salario é "+ salario);
+    }
+}
